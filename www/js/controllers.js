3cf986a6f3bb83bcdd1e362806c343db6e979192
Matheus <mathe_oare@hotmail.com>
2017-02-16 11:42:46 -0300

    Add função de login, puxando dados do servidor

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -20,7 +20,7 @@ angular.module('projeto.controller', [])
 			self.c = resp.results;
 		});
 
-	}]).controller('LoginCtrl', ['$timeout','$location','$ionicScrollDelegate','GetLogin','$ionicPopup', function ($timeout, $location,$ionicScrollDelegate, GetLogin, $ionicPopup) {
+	}]).controller('LoginCtrl', ['$timeout','$location','$ionicScrollDelegate','GetLogin', function ($timeout, $location,$ionicScrollDelegate, GetLogin) {
 		var self = this;
 
 		self.login = 'logo';
@@ -28,26 +28,15 @@ angular.module('projeto.controller', [])
 		self.entrar = false;
 		self.name = true;
 
-
 		self.logar = function (usuario) {
-			console.log(usuario);
-			self.dados = GetLogin.data;
-			self.dados(usuario).success(function (resp) {
-			console.log(resp);
-			token = resp.token;		
-							}).error(function (data, status) {
-							console.log(data);
-							console.log(status);
-								if (status == 400){
-									$ionicPopup.alert({
-										title: 'Erro!',
-										template: 'Login ou senha inválidos!'
-									});
-								}
-							});
+				GetLogin.data().success(function (res) {
+					token = res.token;
+					console.log(token);
+				});
 		}
 
 		self.scrollMainToTop = function () {
 			$ionicScrollDelegate.$getByHandle('mainScroll').scrollTop();
 		};
+		
 	}]);
